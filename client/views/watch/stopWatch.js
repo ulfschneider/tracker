95561ba5f7f8c3f107e2ad89fee0ca5221ca1148
ulfschneider <u@ulf.codes>
2017-05-10 18:08:53 +0200

    Remove watch lock for ios

--- client/views/watch/stopWatch.js
+++ client/views/watch/stopWatch.js
@@ -18,43 +18,13 @@ Meteor.stopWatch = {
             $("#time").html(time);
         }
     },
-    _isIos: function() {
-        return /AppleWebKit/.test(navigator.userAgent) && /Mobile\/\w+/.test(navigator.userAgent);
-    },
-    _awakeLock: function() {
-        /*
-         The idea of the hack is the following:
-         If a new page is requested in safari, the ios device will reset the sleep timeout.
-
-         There will be a request each 15 seconds to a page, so the ios device will not be put to sleep,
-         and the request will be cancelled, so you don't navigate away from the page.
-         http://stackoverflow.com/questions/18905413/how-can-i-prevent-iphone-including-ios-7-from-going-to-sleep-in-html-or-js
-         */
-        if (this._isIos()) {
-            this._clearAwakeLock();
-            this.stayAwakeInterval = Meteor.setInterval(function () {
-                window.location.href = "/";
-                Meteor.setTimeout(window.stop, 0);
-            }, 15000);
-        }
-    },
-    _clearAwakeLock: function() {
-        if (this._isIos()) {
-            if (this.stayAwakeInterval) {
-                Meteor.clearInterval(this.stayAwakeInterval);
-                this.stayAwakeInterval = null;
-            }
-        }
-    },
     start: function () {
         this.startTime = new Date();
         this._setTime();
         this.interval = Meteor.setInterval(this._setTime, 100);
         Session.set("watch", "running");
-        this._awakeLock();
     },
     stop: function () {
-        this._clearAwakeLock();
         Meteor.clearInterval(this.interval);
         Session.set("watch", "stopped");
         this._setTime();
@@ -68,12 +38,10 @@ Meteor.stopWatch = {
         }
     },
     setTime: function () {
-        this._clearAwakeLock();
         Session.set("watchTime", this.millis);
         this.setWatchModeOff();
     },
     clear: function () {
-        this._clearAwakeLock();
         Session.set("watch", "cleared");
         Session.set("watchTime", 0);
         this.millis = 0;
